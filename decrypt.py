484ed747f24d357b9b76b7aae50a8eeacf7125bb
sridhars650 <164981605+sridhars650@users.noreply.github.com>
2024-08-04 12:52:19 -0700

    Encrypt/ Decrypt Methods Added
    more methods to encrypt and decrypt added. more encoding and decoding logic so no more long elif statements. a new private way to encrypt and decrypt with Fernet!

--- decrypt.py
+++ decrypt.py
@@ -1,14 +1,40 @@
 import os
+import time
 import base64
+from cryptography.fernet import Fernet
+
 os.system("clear")
 input("Welcome to the Decryption Chamber! Press enter to continue... ")
 os.system("clear")
 encryptedKey = input("Please input your key to decrypt -> ")
-choice = int(input("Please input if you want (1)base64 or (2)a85. Type in an integer -> "))
-if (choice == 1):
-    decryptedKey = base64.b64decode(encryptedKey).decode()
-elif (choice == 2):
-    decryptedKey = base64.a85decode(encryptedKey).decode()
+
+
+choice = int(input("Please select the decryption method. \n\t1. base64\n\t2. a85 \n\t3. base16\n\t4. base32\n\t5. base32hex\n\t6. Fernet\n\n Type in an integer -> "))
+
+
+decode_methods = {
+    1: base64.b64decode,
+    2: base64.a85decode,
+    3: base64.b16decode,
+    4: base64.b32decode,
+    5: base64.b32hexdecode
+}
+
+# logic method
+def decryptIt(decryptMethod, encryptedKey):
+    return decryptMethod(encryptedKey).decode('utf-8')
+
+if choice in decode_methods:
+    decryptedKey = decryptIt(decode_methods[choice], encryptedKey.encode())
+elif choice == 6: #fernet debug
+    fernetKey = input("Please input the Fernet key for Fernet decryption -> ")
+    f = Fernet(fernetKey.encode('utf-8'))
+    decryptedKey = f.decrypt(encryptedKey.encode('utf-8')).decode('utf-8')
+else:
+    print("Error, retrying in 3 seconds...")
+    time.sleep(3)
+    os.system("python3 decrypt.py")
+    exit()
 
 os.system("clear")
 print(f"Thank you for using the encryption machine. Here is your decrypted key:\n\n------------------------------------------\n\n{decryptedKey}\n\n\n")
--- encrypt.py
+++ encrypt.py
@@ -1,16 +1,40 @@
 import os
+import time
 import base64
+from cryptography.fernet import Fernet
+
 os.system("clear")
 input("Welcome to the Encryption Chamber! Press enter to continue... ")
 os.system("clear")
+
+key = Fernet.generate_key()
+f = Fernet(key)
+
 unEncryptedKey = input("Please input your key to encrypt -> ")
 
-choice = int(input("Please input if you want (1)base64 or (2)a85. Type in an integer -> "))
-if (choice == 1):
-    encryptedKey = base64.b64encode(bytes(unEncryptedKey, 'utf-8')) 
-    encryptedKey = encryptedKey.decode('utf-8') 
-elif (choice == 2):
-    encryptedKey = base64.a85encode(bytes(unEncryptedKey, 'utf-8'))
-    encryptedKey = encryptedKey.decode('utf-8')
+choice = int(input("Please select the encryption method. \n\t1. base64\n\t2. a85 \n\t3. base16\n\t4. base32\n\t5. base32hex\n\t6. Fernet\n\n Type in an integer -> "))
+
+encode_methods = {
+    1: base64.b64encode,
+    2: base64.a85encode,
+    3: base64.b16encode,
+    4: base64.b32encode,
+    5: base64.b32hexencode
+}
+
+def encryptIt(encryptMethod, unEncryptedKey):
+    return encryptMethod(bytes(unEncryptedKey, 'utf-8')).decode('utf-8')
+
+if choice in encode_methods:
+    encryptedKey = encryptIt(encode_methods[choice], unEncryptedKey)
+elif choice == 6:
+    encryptedKey = f.encrypt(bytes(unEncryptedKey, "utf-8")).decode('utf-8')
+else:
+    print("Error, retrying in 3 seconds...")
+    time.sleep(3)
+    os.system("python3 encrypt.py")
+    exit()
+
 os.system("clear")
-print(f"Thank you for using the encryption machine. Here is your encrypted key\n\n------------------------------------------\n\n{encryptedKey}\n\n\n")
+print(f"Thank you for using the encryption machine. Here is your encrypted key:\n\n------------------------------------------------------------\n\n{encryptedKey}\n\n------------------------------------------------------------")
+print(f"\nSave this key for decryption: {key.decode()}\n\n------------------------------------------------------------\n")
