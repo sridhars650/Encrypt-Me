f49b79345a836fee0fbf5b9328c1db4c828f077a
sridhars650 <164981605+sridhars650@users.noreply.github.com>
2024-08-04 16:47:09 -0700

    encryption website completed functionality

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import base64
+from cryptography.fernet import Fernet
 
 app = Flask(__name__)
 
@@ -6,9 +8,46 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/encrypt')
+encode_methods = {
+    1: base64.b64encode,
+    2: base64.a85encode,
+    3: base64.b16encode,
+    4: base64.b32encode,
+    5: base64.b32hexencode
+}
+decode_methods = {
+    1: base64.b64decode,
+    2: base64.a85decode,
+    3: base64.b16decode,
+    4: base64.b32decode,
+    5: base64.b32hexdecode
+}
+
+def encryptIt(encryptMethod, unEncryptedKey):
+    return encryptMethod(bytes(unEncryptedKey, 'utf-8')).decode('utf-8')
+
+# logic method
+def decryptIt(decryptMethod, encryptedKey):
+    return decryptMethod(encryptedKey).decode('utf-8')
+
+@app.route('/encrypt', methods=['GET', 'POST'])
 def encryptPage():
-    return render_template('encrypt.html')
+    encryptedKey = None # default value
+    fernet_key = None # default value
+    if request.method == 'POST': # if post was recieved
+        key = request.form['key'] # add key form param to var key
+        method = int(request.form['method']) # same thing with method but convert to int
+        fernet_key = Fernet.generate_key() # generates a secure fernet key
+        if method in encode_methods: # detects method
+            encryptedKey = encryptIt(encode_methods[method], key) # does encryption
+        elif method == 6: # special case for fernet encryption
+            if fernet_key: # if there is the key
+                f = Fernet(fernet_key.encode('utf-8')) # encodes it
+                encryptedKey = f.encrypt(bytes(key, "utf-8")).decode('utf-8') # using key, it encrypts
+            else:
+                encryptedKey = "Fernet key is required for Fernet encryption." # else it gives error
+
+    return render_template("encrypt.html", encryptedKey=encryptedKey, fernet_key=fernet_key.decode()) # returns with variables
 
 @app.route('/decrypt')
 def decryptPage():
